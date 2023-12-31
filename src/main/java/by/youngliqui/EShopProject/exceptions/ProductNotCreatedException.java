3d23198e07e6362756fcd528d88b5f59c407a9d0
youngliqui <super.pav-79@yandex.by>
2023-12-31 17:30:42 +0300

    product add to the db

---
+++ src/main/java/by/youngliqui/EShopProject/exceptions/ProductNotCreatedException.java
@@ -0,0 +1,7 @@
+package by.youngliqui.EShopProject.exceptions;
+
+public class ProductNotCreatedException extends RuntimeException{
+    public ProductNotCreatedException(String message) {
+        super(message);
+    }
+}
