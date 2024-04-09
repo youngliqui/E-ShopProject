bd573aed9ab722036391436f985e6bd427326d61
youngliqui <super.pav-79@yandex.by>
2024-04-09 13:41:19 +0300

    Revert "Revert "added pagination, sorting and filter for products""
    This reverts commit 9d4831ee76c29d713194335e69c5269bbff86203.

--- src/main/java/by/youngliqui/EShopProject/repositories/ProductRepository.java
+++ src/main/java/by/youngliqui/EShopProject/repositories/ProductRepository.java
@@ -1,9 +1,23 @@
 package by.youngliqui.EShopProject.repositories;
 
 import by.youngliqui.EShopProject.models.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findAllByTitleContainingIgnoreCaseOrderByPriceAsc(String title, Pageable pageable);
+
+    Page<Product> findAllByTitleContainingIgnoreCaseOrderByPriceDesc(String title, Pageable pageable);
+
+    Page<Product> findAllByTitleContainingIgnoreCase(String title, Pageable pageable);
+
+    @Query("SELECT p FROM Product p ORDER BY p.price ASC")
+    Page<Product> findAllSortedByAscendingPrice(Pageable pageable);
+
+    @Query("SELECT p FROM Product p ORDER BY p.price DESC")
+    Page<Product> findAllSortedByDescendingPrice(Pageable pageable);
 }
--- src/main/java/by/youngliqui/EShopProject/services/ProductService.java
+++ src/main/java/by/youngliqui/EShopProject/services/ProductService.java
@@ -6,8 +6,14 @@ import java.util.List;
 
 public interface ProductService {
     List<ProductDTO> getAll();
+
     void addToUserBucket(Long productId, String username);
+
     void addProduct(ProductDTO productDTO);
+
     ProductDTO getById(Long id);
+
     void deleteProductById(Long productId);
+
+    List<ProductDTO> getFilteredAndSortedProducts(String title, String sortBy, int page, int pageSize);
 }
